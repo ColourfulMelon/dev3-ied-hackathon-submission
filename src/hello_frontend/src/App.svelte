7e481c6ba783424dbedd7c8ff92398242c54907a
Jordyn Nitch <NTCJOR001@myuct.ac.za>
2024-02-07 10:30:18 +0200

    Added frontend

---
+++ src/hello_frontend/src/App.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+    import SubscriptionList from './subscriptionList.svelte'
+
+    let myComponent = false;
+    let componentTitle = '';
+    switch (componentTitle) {
+        case '':
+            myComponent = false;
+            break;
+        default:
+            myComponent = true;
+            break;
+    }
+
+
+
+</script>
+
+<main>
+    <div class="header">
+<!--todo: add logo-->
+        <div class="logo-div">
+            <img src="/public/vite.svg">
+            <h1>Subscriptions</h1>
+        </div>
+        <div class="auth">
+            <button id="login-button"> <img id="login" src="/public/IC_logo.png" width="50px"><p id="login">Login</p></button>
+<!--            <p id="principal"></p>-->
+        </div>
+    </div>
+    <div class="body">
+        <SubscriptionList />
+
+
+
+
+    </div>
+
+</main>
+
+<style>
+.body {
+    display: grid;
+
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 5rem;
+    width: 80vw;
+    /*height: 20vh;*/
+
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    /*width: 100vw;*/
+    align-items: center;
+}
+
+.logo-div {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1rem;
+    width: 50vw;
+}
+
+#login {
+    color: #000000;
+
+}
+
+#login-button {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem 2rem;
+    border-radius: 1rem;
+    background-color: #f0f0f0;
+    border: none;
+    cursor: pointer;
+}
+</style>
